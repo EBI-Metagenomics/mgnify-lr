4bd0087204bd2aa643dd20c773206a0d6bf02256
Juan Caballero <juan.caballero@databiology.com>
2020-11-23 14:13:54 -0600

    script to download reference data

---
+++ cwl/db/getDB.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+wget ftp://ftp.ensembl.org/pub/current_fasta/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz
+wget ftp://ftp.ensembl.org/pub/current_fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
