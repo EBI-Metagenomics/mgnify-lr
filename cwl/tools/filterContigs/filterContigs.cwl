39119e2ee1492d9e2522e9a01ce43805b6f27355
Juan Caballero <juan.caballero@databiology.com>
2020-12-02 13:05:56 -0600

    filter contigs by size

---
+++ cwl/tools/filterContigs/filterContigs.cwl
@@ -0,0 +1,57 @@
+cwlVersion: v1.1
+class: CommandLineTool
+label: filterContigs from a Fasta.
+doc: |
+      Implementation of filtering sequence by mininal size in a Fasta file.
+
+requirements:
+  ResourceRequirement:
+    coresMin: 1
+    ramMin: 2000 # 1 GB for testing, it needs more in production
+hints:
+  DockerRequirement:
+    dockerPull: jcaballero/mgnify-lr.filtercontigs:0.0.1
+
+baseCommand: [ "filterContigs.pl" ]
+
+inputs:
+  inFasta:
+    type: File
+    format: edam:format_1929 
+    label: fasta file to filter
+    inputBinding:
+      position: 1
+      prefix: -i
+  minSize:
+    type: int
+    label: mininal size
+    inputBinding:
+      position: 2
+      prefix: -s
+  outFasta:
+    type: string
+    label: output fasta file name
+    inputBinding:
+      position: 2
+      prefix: -o
+ 
+outputs:
+  outFasta:
+    type: File
+    format: edam:format_1929
+    outputBinding:
+      glob: $(inputs.outFasta)
+  
+stdout: filterContigs.log
+stderr: filterContigs.err
+
+$namespaces:
+ edam: http://edamontology.org/
+ s: http://schema.org/
+$schemas:
+ - http://edamontology.org/EDAM_1.16.owl
+ - https://schema.org/version/latest/schemaorg-current-https.rdf
+
+s:license: "https://www.apache.org/licenses/LICENSE-2.0"
+s:copyrightHolder: "EMBL - European Bioinformatics Institute"
+s:dateCreated: 2020-10-08
---
+++ cwl/tools/filterContigs/filterContigs.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+# filterContigs.pl --szie [int] --in [input file] --out [output file]
+# script to filter contigs by size in a sequence file (Fasta)
+# (C) 2020 EMBL-EBI
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+my $infile  = undef;
+my $minsize = undef;
+my $outfile = undef;
+my $total   = 0;
+my $pass    = 0;
+my $ih;
+my $oh;
+
+GetOptions ("size=i"   => \$minsize,
+            "in=s"     => \$infile,
+            "out=s"    => \$outfile)
+or die("Usage: filterContigs.pl --size [int] --in [input file] --out [output file]\n");
+
+if ( !($infile) or !($minsize) or !($outfile) ) {
+    die("Usage: filterContigs.pl --size [int] --in [input file] --out [output file]\n");
+}
+
+
+# Check if input file is compressed
+if ($infile =~ /gz$/i) {
+    open($ih, "-|", "gunzip", "-c", $infile) or die ("cannot read compressed file $infile\n");
+}
+else {
+    open($ih, "<", $infile) or die ("cannot read compressed file $infile\n");
+}
+# check if we want a compressed output
+if ($outfile =~ /gz$/i){
+    open($oh, "|-", "gzip > $outfile") or die ("cannot write compressed file $outfile\n");
+}
+else {
+    open ($oh, ">", $outfile) or die ("cannot write output to $outfile\n");
+}
+
+# Read sequences and filter
+$/ = "\n>";
+while (<$ih>) {
+    s/>//g;
+    $total++;
+    my ($id, @seq) = split (/\n/, $_);
+    my $seq = join "", @seq;
+    my $len = length $seq;
+    if ($len >= $minsize) {
+        print $oh join "\n", ">$id", @seq;
+        $pass++;
+    }
+}
+close $ih;
+close $oh;
+# quik report
+my @path = split(/\//, $infile);
+warn "Input file:  $path[-1]\n";
+warn "Min size:    $minsize\n";
+warn "Sequences:   $total\n";
+warn "Passed seqs: $pass\n";
