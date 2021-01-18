a61562efe405058f7bd7197e55a37725cb40354f
Juan Caballero <juan.caballero@databiology.com>
2021-01-18 10:37:39 -0600

    script to compute mean coverage

---
+++ cwl/utils/meanCov.pl
@@ -0,0 +1,17 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+
+my $sum = 0;
+my $num = 0;
+while (<>) {
+    next if (/^#/);
+    chomp;
+    $num++;
+    my @ln = split(/\s+/, $_);
+    $sum += $ln[5];
+}
+
+my $mean = sprintf("%.1f", $sum / $num);
+print "$mean\n";
