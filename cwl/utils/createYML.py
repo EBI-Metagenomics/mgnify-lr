15efe0abdd15958d5b75a9c757e563b8adb8565a
Juan Caballero <juan.caballero@databiology.com>
2021-01-20 14:37:22 -0600

    updating config script

--- cwl/utils/createYML.py
+++ cwl/utils/createYML.py
@@ -6,7 +6,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Generate the YAML file for MGnify-LR pipeline')
-parser.add_argument('-m', '--mode', type=str, choices=['assembly', 'hybrid', 'polish'], help='Pipeline mode selection, default: assembly', default='assembly')
+parser.add_argument('-m', '--mode', type=str, choices=['assembly', 'hybrid'], help='Pipeline mode selection, default: assembly', default='assembly')
 parser.add_argument('-o', '--out', type=str, help='Output YAML file', required=True)
 parser.add_argument('-r', '--reads', type=str, help='Path to long reads Fastq file', required=True)
 parser.add_argument('-t', '--tech', type=str, choices=['nanopore', 'pacbio'], help='Long-reads techonology', required=True)
@@ -22,7 +22,7 @@ parser.add_argument('-u', '--uniprot', type=str, help='Path to Uniprot file (dia
 
 par = parser.parse_args()
 
-if par.mode == "hybrid" or par.mode == "polish":
+if par.mode == "hybrid":
     if not par.pair1:
         print("Mode is '{}', missing Illumina pair 1 fastq (-1/--pair1)".format(par.mode))
         exit(1)
@@ -34,35 +34,32 @@ oh = open(par.out, "w")
 
 oh.write("raw_reads:\n  class: File\n  format: edam:format_1930\n  path: {}\n".format(par.reads))
 oh.write("min_read_size: {}\n".format(par.minLen))
+oh.write("raw_reads_report: {}\n".format(par.prefix + "_raw_reads_stats.txt"))    
 oh.write("min_contig_size: {}\n".format(par.minContig))
 oh.write("lr_tech: {}\n".format(par.tech))
-oh.write("raw_reads_report: {}\n".format(par.prefix + "_raw_reads_stats.txt"))    
+
+if par.tech == "pacbio":
+    oh.write("align_polish: map-pb\n")
+else:
+    oh.write("align_polish: map-ont\n")
 
 if par.mode == "assembly":
-    oh.write("reads_filter_bysize_name: {}\n".format(par.prefix + "_filtered_reads"))
+    oh.write("reads_filter_bysize: {}\n".format(par.prefix + "_filtered_reads"))
     if par.host: # assembly with Host filtering
         oh.write("host_genome:\n  class: File\n  format: edam:format_1929\n  path: {}\n".format(par.host))
-        oh.write("host_unmaped: {}\n".format(par.prefix + "_filterHost.fastq.gz"))
-        oh.write("polish_assembly_racon: {}\n".format(par.prefix + "_racon.fasta"))
-        oh.write("host_unmaped_contigs: {}\n".format(par.prefix + "_unmap.fasta"))
-    else: # assembly without Host filtering
-        oh.write("polish_assembly_racon: {}\n".format(par.prefix + "_racon.fasta"))
-    oh.write("polish_paf: {}\n".format(par.prefix + "_polish.paf"))
-    oh.write("medaka_model: {}\n".format(par.medakaModel))
+        if par.tech == "pacbio":
+            oh.write("align_preset: map-pb\n")
+        else:
+            oh.write("align_preset: map-ont\n")
 
-elif par.mode == "polish":
-    oh.write("p1_reads:\n  class: File\n  format: edam:format_1930\n  path: {}\n".format(par.pair1))
-    oh.write("p2_reads:\n  class: File\n  format: edam:format_1930\n  path: {}\n".format(par.pair2))
-    oh.write("min_read_size: {}\n".format(par.minLen))
-    oh.write("reads_filter_bysize_name: {}\n".format(par.prefix + "_filtered_reads"))
-    if par.host: # assembly with Host filtering and Illumina polishing
-        oh.write("host_genome:\n  class: File\n  format: edam:format_1929\n  path: {}\n".format(par.host))
-        oh.write("host_unmaped: {}\n".format(par.prefix + "_filterHost.fastq.gz"))
-        oh.write("host_unmaped_contigs: {}\n".format(par.prefix + "_unmapHost.fasta"))
-    oh.write("polish_assembly_racon: {}\n".format(par.prefix + "_racon.fasta"))
+    else: # assembly without Host filtering
+        oh.write("align_preset: none\n")
+
+    oh.write("host_unmaped_reads: {}\n".format(par.prefix + "_filterHost.fastq.gz"))
     oh.write("polish_paf: {}\n".format(par.prefix + "_polish.paf"))
+    oh.write("polish_assembly_racon: {}\n".format(par.prefix + "_racon.fasta"))
+    oh.write("host_unmaped_contigs: {}\n".format(par.prefix + "_unmap.fasta"))
     oh.write("medaka_model: {}\n".format(par.medakaModel))
-    oh.write("polish_assembly_pilon: {}\n".format(par.prefix + "_pilon"))
 
 elif par.mode == "hybrid":
     oh.write("p1_reads:\n  class: File\n  format: edam:format_1930\n  path: {}\n".format(par.pair1))
@@ -84,9 +81,7 @@ else:
     exit(1)
 
 oh.write("final_assembly: {}\n".format(par.prefix + "_final.fasta"))
-oh.write("final_assembly_stats: {}\n".format(par.prefix + "_final_stats.txt"))
 oh.write("predict_proteins: {}\n".format(par.prefix + "_prot.fasta"))
-oh.write("predict_proteins_gbk: {}\n".format(par.prefix + "_prot.gbk"))
 oh.write("uniprot_index:\n  class: File\n  path: {}\n".format(par.uniprot))
 oh.write("diamond_out: {}\n".format(par.prefix + "_diamond.tsv"))
 oh.write("ideel_out: {}\n".format(par.prefix + "_ideel.pdf"))
