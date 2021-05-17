3e84a8ae1f732fe7e584768b167c9025bc4afe86
Juan Caballero <jcaballero@ebi.ac.uk>
2021-05-17 13:56:20 -0500

    help editing

--- cwl/utils/assembly_longreads.sh
+++ cwl/utils/assembly_longreads.sh
@@ -14,6 +14,7 @@ MEDAKAMODEL=r941_min_high_g360
 MINILL=50
 MINLR=200
 MINCONTIG=500
+RESTART=False
 
 print_help () {
     echo "assembly_longreads.sh [PARAM]"
@@ -21,14 +22,14 @@ print_help () {
     echo "Parameter |  Definition"
     echo "----------|------------"
     echo "   -p     |  ProjectID (SRPxxx|ERPxxx|DRPxxx)"
-    echo "   -r     |  List or runs IDs (SRRxxx|ERRxxx|DRRxxx) to be assembled, if ommited, it assemble all runs in the project /raw directory"
+    echo "   -r     |  List or runs IDs (SRRxxx|ERRxxx|DRRxxx) to be assembled, if omitted, it assemble all runs in the project /raw directory"
     echo "   -t     |  Long-read technology: nanopore or pacbio"
-    echo "   -g     |  Path to genome fasta file"
-    echo "   -m     |  Medaka polish model, default $MEDAKAMODEL"
-    echo "   -i     |  Minimal size for Illumina filtering, default $MINILL"
-    echo "   -j     |  Minimal size for long-read filtering, default $MINLR"
-    echo "   -k     |  Minimal size for contig filtering, default $MINCONTIG"
-    echo "   -d     |  Assembly project directory, default $TARGETDIR"
+    echo "   -g     |  Path to genome fasta file for host filtering"
+    echo "   -m     |  Medaka polish model for Nanopore, see https://github.com/nanoporetech/medaka#models, default is $MEDAKAMODEL"
+    echo "   -j     |  Minimum size for long-read filtering, default is $MINLR"
+    echo "   -k     |  Minimum size for contig filtering, default is $MINCONTIG"
+    echo "   -d     |  Assembly project base directory, default is $TARGETDIR"
+    echo "   -x     |  Restart mode, should be True or False, default is $RESTART"
 }
 
 # simple error printing and script exit
@@ -42,11 +43,11 @@ launch_job () {
     FQ=$1
     NAME=$(basename $FQ .fastq.gz)
     echo "Launching assembly job $NAME"
-    bsub -q production-rh74 -e $RAWDIR/../$NAME.err -o $RAWDIR/../$NAME.log -J $NAME "bash $LAUNCHER -p $PROJECT -s $FQ -h $TECH -t assembly -g $GENOME -k $MEDAKAMODEL -i $MINILL -j $MINLR -c $MINCONTIG"
+    bsub -q production-rh74 -e $RAWDIR/../$NAME.err -o $RAWDIR/../$NAME.log -J $NAME "bash $LAUNCHER -x $RESTART -p $PROJECT -s $FQ -h $TECH -t assembly -g $GENOME -k $MEDAKAMODEL -i $MINILL -j $MINLR -c $MINCONTIG"
 }
 
 # Parameter capture
-while getopts :p:r:t:m:i:j:k:g:d: option; do
+while getopts :p:r:t:m:i:j:k:g:d:x: option; do
     case "${option}" in
         p) PROJECT=${OPTARG};;
         r) RUNS=${OPTARG};;
@@ -57,6 +58,7 @@ while getopts :p:r:t:m:i:j:k:g:d: option; do
         k) MINCONTIG=${OPTARG};;
         g) GENOME=${OPTARG};;
         d) TARGETDIR=${OPTARG};;
+        x) RESTART=${OPTARG};;
         *) echo "invalid option: $option"; exit;;
     esac
 done
